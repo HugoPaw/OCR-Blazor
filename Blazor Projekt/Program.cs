465295fe4b03aa568172a477936dfc20972d0989
Hugo Code <hugoocode@gmail.com>
2025-05-05 23:29:42 +0200

    Aktuellen Stand der Wetterseite und OCR-Funktionen

--- Blazor Projekt/Program.cs
+++ Blazor Projekt/Program.cs
@@ -1,23 +1,31 @@
-using BlazorProjekt.Components;
+﻿using BlazorProjekt.Components;
 using BlazorProjekt.Services;
 using Microsoft.AspNetCore.Http.Features;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Razor Components + Blazor Server + Fehlerdetails aktivieren
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents()
+    .AddCircuitOptions(options => options.DetailedErrors = true);
 
+// Upload-Limit erhöhen
 builder.Services.Configure<FormOptions>(options =>
 {
-    options.MultipartBodyLengthLimit = 15 * 1024 * 1024;
+    options.MultipartBodyLengthLimit = 15 * 1024 * 1024; // 15 MB
 });
 
+// OCR-bezogene Services registrieren
 builder.Services.AddScoped<PdfTextExtractor>();
 builder.Services.AddScoped<ImageTextExtractor>();
 builder.Services.AddScoped<OcrService>();
 
+// HttpClient aktivieren (z. B. für Wetter-API)
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
+// Fehlerbehandlung und Sicherheitsfeatures
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -28,6 +36,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+// Razor-Komponenten in Blazor Server aktivieren
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
