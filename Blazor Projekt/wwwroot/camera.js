f431d2747009428213fa99808a45ac4f2c4a58d7
Hugo Code <hugoocode@gmail.com>
2025-05-12 00:45:58 +0200

    pokemon card scanner

--- Blazor Projekt/wwwroot/camera.js
+++ Blazor Projekt/wwwroot/camera.js
@@ -1,1 +1,20 @@
-﻿
+﻿window.openCameraAndGetImage = async function () {
+    return new Promise((resolve, reject) => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'image/*';
+        input.capture = 'environment'; // bevorzugt Rückkamera bei Mobilgeräten
+
+        input.onchange = () => {
+            const file = input.files[0];
+            const reader = new FileReader();
+
+            reader.onload = () => resolve(reader.result); // gibt base64 zurück
+            reader.onerror = error => reject(error);
+
+            reader.readAsDataURL(file);
+        };
+
+        input.click();
+    });
+};
