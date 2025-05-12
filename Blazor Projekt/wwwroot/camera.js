2ba3627109d78cb13bfd3ad4fb0276d43d9985a3
Hugo Code <hugoocode@gmail.com>
2025-05-12 21:23:19 +0200

    update homepage add code claner

--- Blazor Projekt/wwwroot/camera.js
+++ Blazor Projekt/wwwroot/camera.js
@@ -4,17 +4,13 @@
         input.type = 'file';
         input.accept = 'image/*';
         input.capture = 'environment'; // bevorzugt Rückkamera bei Mobilgeräten
-
         input.onchange = () => {
             const file = input.files[0];
             const reader = new FileReader();
-
             reader.onload = () => resolve(reader.result); // gibt base64 zurück
             reader.onerror = error => reject(error);
-
             reader.readAsDataURL(file);
         };
-
         input.click();
     });
 };
