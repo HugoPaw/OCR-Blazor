04342680e4957f0da7ff7eb73134c88dfc977a50
Hugo Code <hugoocode@gmail.com>
2025-06-11 00:07:11 +0200

    darkmode

---
+++ Blazor Projekt/wwwroot/darkmode.js
@@ -0,0 +1,12 @@
+﻿console.log("✅ darkmode.js wurde geladen");
+
+window.toggleDarkMode = function (isDark) {
+    const main = document.getElementById("main-content");
+    if (!main) return;
+
+    if (isDark) {
+        main.classList.add("dark-mode");
+    } else {
+        main.classList.remove("dark-mode");
+    }
+};
